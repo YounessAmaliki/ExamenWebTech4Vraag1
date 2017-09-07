b9a755ca9bdefe3cc153f7aa5be393a40838be67
Amaliki Youness <youness.amaliki@student.ap.be>
2017-09-07 15:08:20 +0200

    Adding year movie
    Productiejaar ophalen van movie

--- src/edu/ap/rd/quotes/movieResource.java
+++ src/edu/ap/rd/quotes/movieResource.java
@@ -85,15 +85,27 @@ public class movieResource {
 					actorsList.add(quote);
 				}
 				
+				for (String year : jedis.smembers("year:" + id)) {
+					actorsList.add(year);
+				}
+				
 			}
 		}
 		
+
+
 		
 		for(String quote: actorsList){
 			
 			builder.append("<li>"+quote+"</li>");
 			
 		}
+		
+		
+		
+		
+		
+		
 		builder.append("<br><br><a href=/ExamenWebTech4Vraag1/index/>Home</a>");
 				
 		builder.append("</body>");
@@ -133,79 +145,7 @@ public class movieResource {
 		return builder.toString();
 	}
 
-/*
-	@GET
-	@Produces({ "text/html" })
-	public String getAllactors() {
-		// Uncomment voor opvullen
-		this.fillDb(true);
 
-		Jedis jedis = JedisConnection.getInstance().getConnection();
-		StringBuilder builder = new StringBuilder();
-
-		builder.append("<html>");
-		builder.append("<head>");
-		builder.append("<title>actors</title>");
-		builder.append("</head>");
-
-		builder.append("<body>");
-		builder.append("<ul>");
-		for (String key : jedis.keys("actors:*")) {
-			for (String quote : jedis.smembers(key)) {
-				builder.append("<li>" + quote);
-			}
-		}
-		builder.append("</ul>");
-		builder.append("</body>");
-		builder.append("</html>");
-
-		return builder.toString();
-	}
-
-/*
-	// Specifieke auteur
-	// Data => de data die wordt gepost
-	@POST
-	public String getmovieactors(String data) {
-		// Uncomment voor opvullen
-		//this.fillDb(true);
-
-		Jedis jedis = JedisConnection.getInstance().getConnection();
-		StringBuilder builder = new StringBuilder();
-
-		builder.append("<html>");
-		builder.append("<head>");
-		builder.append("<title>actors</title>");
-		builder.append("</head>");
-
-		builder.append("<body>");
-		// Selecteren van correcte movie
-		for (String movie : jedis.keys("movie:*")) {
-			String tmpmovie = jedis.get(movie);
-			if (tmpmovie.equals(data)) {
-				builder.append("testink");
-				int movieId = Integer.parseInt(movie.split(":")[1]);
-
-				builder.append("<h1>" + tmpmovie + "</h1>");
-				builder.append("<ul>");
-				for (String quote : jedis.smembers("actors:" + movieId)) {
-					builder.append("<li>" + quote);
-				}
-				builder.append("</ul>");
-
-				break; 
-			}
-		}
-
-		builder.append("</body>");
-		builder.append("</html>");
-
-		return builder.toString();
-	}
-
-
-
-	*/
 	private void fillDb(boolean flush) {
 		Jedis jedis = JedisConnection.getInstance().getConnection();
 
@@ -218,6 +158,7 @@ public class movieResource {
 
 		jedis.sadd("actors:1", "Wolverine");
 		jedis.sadd("actors:1", "Batman");
+		jedis.sadd("year:1", "2016");
 		
 	}
 
